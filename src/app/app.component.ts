52d196fc591b09318901f3517995b183f81ebc2a
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2019-09-27 18:38:36 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tot-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tour-of-thrones';
+}
